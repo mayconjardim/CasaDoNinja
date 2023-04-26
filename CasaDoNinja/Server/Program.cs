2f97e347be2385a84a5b9c574f99d5511ab7b40c
Maycon Jardim <mayconsmill@gmail.com>
2023-04-26 17:50:02 -0300

    Adicionando DataContext e Configurando Database

--- CasaDoNinja/Server/Program.cs
+++ CasaDoNinja/Server/Program.cs
@@ -1,13 +1,20 @@
-global using CasaDoNinja.Shared;
+    global using CasaDoNinja.Shared;
+global using Microsoft.EntityFrameworkCore;
+using CasaDoNinja.Server.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddDbContext<DataContext>(options => options.UseSqlServer
+(builder.Configuration.GetConnectionString("Database")));
 
 builder.Services.AddControllersWithViews();
 builder.Services.AddRazorPages();
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
 
 var app = builder.Build();
 
