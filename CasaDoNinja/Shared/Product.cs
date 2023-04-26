40f1944df4141f901521dacbc253b1240aea1ecc
Maycon Jardim <mayconsmill@gmail.com>
2023-04-26 18:23:15 -0300

    Criando Migração inicial já com seed de Product

--- CasaDoNinja/Shared/Product.cs
+++ CasaDoNinja/Shared/Product.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
 
 namespace CasaDoNinja.Shared
 {
@@ -13,6 +10,7 @@ namespace CasaDoNinja.Shared
         public string Title { get; set; } = string.Empty;
         public string Description { get; set; } = string.Empty;
         public string ImageUrl { get; set; } = string.Empty;
+        [Column(TypeName = "decimal(18,2)")]
         public decimal Price { get; set; }
 
 
