9dedafb5df5f64c42fb7c1e281b3cc8247ef2290
Maycon Jardim <mayconsmill@gmail.com>
2023-04-27 14:50:52 -0300

    Criando Classe ProductType e ProductVariant

---
+++ CasaDoNinja/Shared/ProductType.cs
@@ -0,0 +1,10 @@
+﻿namespace CasaDoNinja.Shared
+{
+    public class ProductType
+    {
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+    }
+}
---
+++ CasaDoNinja/Shared/ProductVariant.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace CasaDoNinja.Shared
+{
+    public class ProductVariant
+    {
+        [JsonIgnore]
+        public Product Product { get; set; }
+        public int ProductId { get; set; }
+
+        [JsonIgnore]
+        public ProductType ProductType { get; set; }
+        public int ProductTypeId { get; set; }
+
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal Price { get; set; }
+
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal OriginalPrice { get; set; }
+
+    }
+}
