e502104054143cdf4f31565b59739b0580297def
Maycon Jardim <mayconsmill@gmail.com>
2023-04-27 11:22:18 -0300

    Criando Classe Category e Fazendo seu Relacionamento com Product

--- CasaDoNinja/Shared/Product.cs
+++ CasaDoNinja/Shared/Product.cs
@@ -12,6 +12,8 @@ namespace CasaDoNinja.Shared
         public string ImageUrl { get; set; } = string.Empty;
         [Column(TypeName = "decimal(18,2)")]
         public decimal Price { get; set; }
+        public Category? Category { get; set; }
+        public int CategoryId { get; set; }
 
     }
 }
