9bb5b9a6cc3c71a1ef39dad9e70447b91e9cbbdd
Maycon Jardim <mayconsmill@gmail.com>
2023-04-27 10:56:05 -0300

    Criando Component ProductDetails

---
+++ CasaDoNinja/Client/Pages/ProductDetails.razor.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace CasaDoNinja.Client.Pages
+{
+    partial class ProductDetails
+    {
+
+        private Product? product = null;
+
+        [Parameter]
+        public int Id { get; set; }
+
+        protected override async Task OnParametersSetAsync()
+        {
+           product = ProductService.Products.Find(p => p.Id == Id);
+        }
+
+    }
+}
--- CasaDoNinja/Shared/Product.cs
+++ CasaDoNinja/Shared/Product.cs
@@ -13,6 +13,5 @@ namespace CasaDoNinja.Shared
         [Column(TypeName = "decimal(18,2)")]
         public decimal Price { get; set; }
 
-
     }
 }
