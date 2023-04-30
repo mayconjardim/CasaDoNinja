3bf3ef6979c6d45166cd8a79202137946722dd43
Maycon Parreira Jardim <mayconsmill@gmail.com>
2023-04-29 23:29:23 -0300

    Adicionando Validação a Register no Client

--- CasaDoNinja/Shared/UserRegister.cs
+++ CasaDoNinja/Shared/UserRegister.cs
@@ -1,15 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace CasaDoNinja.Shared
 {
     public class UserRegister
     {
+        [Required, EmailAddress]
         public string Email { get; set; } = string.Empty;
+        [Required, StringLength(100, MinimumLength = 6)]
         public string Password { get; set; } = string.Empty;
+        [Compare("Password", ErrorMessage ="As senhas estão diferentes!")]
         public string ConfirmPassword { get; set; } = string.Empty;
 
     }
