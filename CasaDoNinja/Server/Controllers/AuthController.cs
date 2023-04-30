ee0bb75b8104a78df180502041e2b2dce05d5e9f
Maycon Parreira Jardim <mayconsmill@gmail.com>
2023-04-30 12:17:08 -0300

    Criando metodo de Login e Gerando com Token com JWT

--- CasaDoNinja/Server/Controllers/AuthController.cs
+++ CasaDoNinja/Server/Controllers/AuthController.cs
@@ -31,5 +31,19 @@ namespace CasaDoNinja.Server.Controllers
 
             return Ok(response);
         }
+
+        [HttpPost("login")]
+        public async Task<ActionResult<ServiceResponse<string>>> Login(UserLogin request)
+        {
+            var response = await _authService.Login(request.Email, request.Password);
+
+            if (!response.Success)
+            {
+                return BadRequest(response);
+            }
+
+            return Ok(response);
+
+        }
     }
 }
